fe6b501426a4006c812bb028b5b37922b732f14a
mason <masonDev108@gmail.com>
2023-05-17 21:15:52 +0900

    RateDiscountPolicy 주석 수정

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 //  이름 그대로 생성자를 통해서 의존 관계를 주입 받는 방법이다.
 //  지금까지 우리가 진행했던 방법이 바로 생성자 주입이다.
-//  특징
 //  생성자 호출시점에 딱 1번만 호출되는 것이 보장된다.
 //  불변, 필수 의존관계에 사용
 public class RateDiscountPolicy implements DiscountPolicy{
