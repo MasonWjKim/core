edda83ec3d1e3c8bffdca2e4ad333aeda22839b2
mason <masonDev108@gmail.com>
2023-05-30 22:19:31 +0900

    다양한 의존관계 주입방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,10 +9,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    // @Autowired private MemberRepository memberRepository;
+    // 필드주입 -> 외부에서 변경 불가능, DI프레임워크없이는 아무것도 하지 못함 ...>> 사용X
+    private DiscountPolicy discountPolicy;
+
+
+    @Autowired(required = false) // 선택적 수정자 주입**
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Autowired
+    // 수정자 주입(setter 주입) - 1. Spring Bean 등록 2. 연관관계 자동주입 (Autowired)
+    // 선택**, 변경* 가능성이 있는 의존관계에 사용
+    // 자바빈 프로퍼티 규약의 수정자 메서드 방식을 사용하는 방법이다.
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    // 생성자 주입 -> Spring Bean을 등록하면서 연관관계를 같이 주입한다. (수정자 주입과 다르게 LifeCycle 1,2 동시 진행)
+    // 중요!! 생성자가 하나만 있으면 Autowired를 생략해도 된다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
